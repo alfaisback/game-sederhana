b5f5b3741cf412ab86860e68dafe965a25bf86e4
alfa <muhamadalfarizy20@gmail.com>
2022-09-25 12:48:27 +0700

    membuat game sederhana

---
+++ index.js
@@ -0,0 +1,51 @@
+// alert('game ini beronde 5 kali tebak angka 1-3 \n jangan menebak angka yang sama , selamat bermain gais'); 
+
+// let scorePlayer1 = 0;
+// let scorePlayer2 = 0;
+// let i;
+
+// const startGame = (angka, pilihanPlayer1, pilihanPlayer2) => {
+//     angka = Math.floor((Math.random() * 3)+ 1);
+//     pilihanPlayer1 = prompt('player 1 masukan angka :');
+//     pilihanPlayer2 =  prompt('player 2 masukan angka :');
+
+    
+//     if (pilihanPlayer1 == angka && pilihanPlayer2 != angka){
+//         alert('player1 menang!');
+//         scorePlayer1++;
+//         alert(`niilai yang dicari adalah ${angka} \n score \n player 1 : ${scorePlayer1} \n player 2 : ${scorePlayer2}`);
+//     } else if (pilihanPlayer1 != angka && pilihanPlayer2 == angka){
+//         alert('player2 menang!');
+//         scorePlayer2++;
+//         alert(`niilai yang dicari adalah ${angka} \n score \n player 1 : ${scorePlayer1} \n player 2 : ${scorePlayer2}`);
+//     } else if (pilihanPlayer1 == pilihanPlayer2){
+//         alert(`pilihan tidak boleh sama`);
+//         alert(`ulangi ??`);
+//         i = 0;
+//     } else { 
+//         alert('tidak ada yang benar dong!');
+//         alert(`niilai yang dicari adalah ${angka} \n score \n player 1 : ${scorePlayer1} \n player 2 : ${scorePlayer2}`);
+//     }
+// }
+
+// for(i = 0; i <= 5; i++){
+//     startGame();
+// }
+
+// if (scorePlayer1 > scorePlayer2){
+//     alert('player 1 Menang');
+// } else if (scorePlayer1 < scorePlayer2){
+//     alert('player 2 Menang');
+// } else {
+//     alert('score draw');
+// }
+for (let i = 1; i.length <= 5; i += 1);
+console.log(i);
+
+
+
+
+
+
+
+
